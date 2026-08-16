58a7a99b9cd69caa16b13ff574078bc74ccddf2e
Sara <sara2743@gmail.com>
2026-08-17 00:30:51 +0600

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -40,7 +40,7 @@ const app = express();
 
 // Security & parsing middleware
 app.use(helmet());
-app.use(cors({ origin: env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(mongoSanitize());
